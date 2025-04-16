713b869fd4d0ce2dfebf851890742e1b425e0442
Danis <dandijar@yandex.ru>
2025-04-16 21:08:21 +0500

    fix(shortener): call GetValidatedId() to get Id in LinkModelFromLinkRequestWithId()

--- shortener/internal/service/helper/converter.go
+++ shortener/internal/service/helper/converter.go
@@ -86,7 +86,7 @@ func LinkModelFromLinkRequestWithId(request LinkBodyRequestWithId, expireAtDefau
 	if err != nil {
 		return nil, fmt.Errorf("error while validating link with id: %v", err)
 	}
-	id, err = GetValidatedUserId(request)
+	id, err = GetValidatedId(request)
 	if err != nil {
 		return nil, fmt.Errorf("error while validating link with id: %v", err)
 	}
