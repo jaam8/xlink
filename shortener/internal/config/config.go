1c0f0f15e7be2d865fb09d901cccbb959dc20398
jaam8 <jaam8@yandex.ru>
2025-04-18 20:27:53 +0700

    fix(shortener, token_service): add env-prefix to config (REDIS, POSTGRES)

--- shortener/internal/config/config.go
+++ shortener/internal/config/config.go
@@ -8,8 +8,8 @@ import (
 )
 
 type Config struct {
-	Redis                        redis.Config    `yaml:"redis" env:"REDIS" env-prefix:""`
-	Postgres                     postgres.Config `yaml:"postgres" env:"POSTGRES" env-prefix:""`
+	Redis                        redis.Config    `yaml:"redis" env-prefix:"REDIS_"`
+	Postgres                     postgres.Config `yaml:"postgres" env-prefix:"POSTGRES_"`
 	GRPCPort                     int             `yaml:"grpc_port" env:"GRPC_PORT" env-default:"50050"`
 	ExpirationSeconds            int             `yaml:"expiration_seconds" env:"EXPIRATION_SECONDS" env-default:"500"`
 	DefaultLinkExpirationMinutes int             `yaml:"default_link_expiration_minutes" env:"DEFAULT_LINK_EXPIRATION_MINUTES" env-default:"14400"`
