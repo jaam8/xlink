6ca7585c5f26b29bf9ef2fbcf4767a8085cf0e05
Danis <dandijar@yandex.ru>
2025-04-21 23:06:57 +0500

    fix(common): use time.Duration instead of uint for baseDelay

--- common/callers/retry.go
+++ common/callers/retry.go
@@ -8,11 +8,11 @@ import (
 // Retry calls a function at most `maxRetries` times, increasing the delay (baseDelay * 2^n for each try)
 //
 // returns an error if all calls fail
-func Retry(operation func() error, maxRetries uint, baseDelay uint) error {
+func Retry(operation func() error, maxRetries uint, baseDelay time.Duration) error {
 	var err error
 	for n := uint(0); n <= maxRetries; n++ {
 		if n > 0 {
-			time.Sleep(time.Duration(baseDelay*uint(math.Pow(2, float64(n)))) * time.Millisecond)
+			time.Sleep(baseDelay * time.Duration(uint(math.Pow(2, float64(n)))))
 		}
 		err = operation()
 		if err == nil {
