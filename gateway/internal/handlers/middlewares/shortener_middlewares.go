41de6b325304451d5c613726a3ec8c0ae893f8dc
Danis <dandijar@yandex.ru>
2025-04-22 03:20:16 +0500

    feat(user_service): shortener owner-only middleware (Requires new gRPC method!)

---
+++ gateway/internal/handlers/middlewares/shortener_middlewares.go
@@ -0,0 +1,23 @@
+package middlewares
+
+import (
+	"fmt"
+	"github.com/gofiber/fiber/v2"
+	"xlink/gateway/internal/handlers/helpers"
+)
+
+type ShortenerOwnerChecker interface {
+}
+
+func ShortenerOwnerOnlyMiddleware(idParamName string, shortenerService ShortenerOwnerChecker) fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		_, err := helpers.ParseUUID(c, idParamName)
+		if err != nil {
+			return helpers.BadRequest(c, fmt.Sprintf("invalid %s", idParamName))
+		}
+
+		// check link owner via GetLink
+
+		return c.Next()
+	}
+}
