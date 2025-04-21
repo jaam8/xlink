84297c04c55fd679651cb38c43699aeb1c63cd14
jaam8 <jaam8@yandex.ru>
2025-04-22 03:08:45 +0700

    fix(user_service/migrations): fix UUID generator function name (use gen_random_uuid)

--- user_service/migrations/000001_create_users_table.up.sql
+++ user_service/migrations/000001_create_users_table.up.sql
@@ -5,7 +5,7 @@ create schema if not exists schema_name;
 create table if not exists schema_name.users
 (
     id          uuid        not null
-        default get_random_uuid()
+        default gen_random_uuid()
         constraint urls_pk
             primary key,
     telegram_id     bigint null,
