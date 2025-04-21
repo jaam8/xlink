8351d3b216145608b2c8fe0845172952ac8c455d
Danis <dandijar@yandex.ru>
2025-04-21 17:51:22 +0500

    feat(common): implement grpc pool (untested)

---
+++ common/grpc/pool/pool_config.go
@@ -0,0 +1,10 @@
+package pool
+
+import "google.golang.org/grpc"
+
+type Config struct {
+	Address        string `json:"address"`
+	MaxConnections int    `json:"max_connections"`
+	MinConnections int    `json:"min_connections"`
+	DialOptions    []grpc.DialOption
+}
