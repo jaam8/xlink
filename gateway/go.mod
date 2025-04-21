fb50de97fd587f05dae6e0ab172c4bccd91cc05a
Danis <dandijar@yandex.ru>
2025-04-21 17:54:00 +0500

    feat(gateway): implement user service adapter (call gRPC methods through GrpcPool)

--- gateway/go.mod
+++ gateway/go.mod
@@ -25,7 +25,12 @@ require (
 	github.com/valyala/fasthttp v1.51.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
+	golang.org/x/net v0.39.0 // indirect
 	golang.org/x/sys v0.32.0 // indirect
+	golang.org/x/text v0.24.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250414145226-207652e42e2e // indirect
+	google.golang.org/grpc v1.71.1 // indirect
+	google.golang.org/protobuf v1.36.6 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	olympos.io/encoding/edn v0.0.0-20201019073823-d3554ca0b0a3 // indirect
 )
---
+++ gateway/internal/ports/ports.go
@@ -0,0 +1,14 @@
+package ports
+
+import "xlink/common/gen/user_service"
+
+type UserServiceRepository interface {
+	CreateUser(request *user_service.CreateUserRequest) (*user_service.CreateUserResponse, error)
+	GetUser(request *user_service.GetUserRequest) (*user_service.GetUserResponse, error)
+	GetUserIDByToken(request *user_service.GetUserIDByTokenRequest) (*user_service.GetUserIDByTokenResponse, error)
+	GetUserIDByTgID(request *user_service.GetUserIDByTgIDRequest) (*user_service.GetUserIDByTgIDResponse, error)
+	UpdateUser(request *user_service.UpdateUserRequest) (*user_service.UpdateUserResponse, error)
+	CheckToken(request *user_service.TokenCheckRequest) (*user_service.TokenCheckResponse, error)
+	RefreshToken(request *user_service.RefreshTokenRequest) (*user_service.RefreshTokenResponse, error)
+	GetRole(request *user_service.GetRoleRequest) (*user_service.GetRoleResponse, error)
+}
