753dc623b0c7ae362e4971a2ae33cb5be490541b
jaam8 <jaam8@yandex.ru>
2025-04-22 14:58:28 +0700

    feat(analytics): add RedirectClickInfo model and add json tags to models

--- analytics/internal/models/click.go
+++ analytics/internal/models/click.go
@@ -3,13 +3,24 @@ package models
 import "time"
 
 type Click struct {
-	LinkID     string
-	LinkOwner  string
-	ClickedAt  time.Time
-	Referrer   string
-	Region     string
-	Browser    string
-	DeviceType string
-	IPAddress  string
-	IsUnique   bool
+	LinkID     string    `json:"link_id"`
+	LinkOwner  string    `json:"link_owner"`
+	ClickedAt  time.Time `json:"clicked_at"`
+	Referrer   string    `json:"referrer"`
+	Region     string    `json:"region"`
+	Browser    string    `json:"browser"`
+	DeviceType string    `json:"device_type"`
+	IPAddress  string    `json:"ip_address"`
+	IsUnique   bool      `json:"is_unique"`
+}
+
+type RedirectClickInfo struct {
+	ShortLink    string    `json:"short_link"`
+	ClickedAt    time.Time `json:"clicked_at"`
+	Referrer     string    `json:"referrer"`
+	IPAddress    string    `json:"ip_address"`
+	VisitorToken string    `json:"visitor_token"`
+	Browser      string    `json:"browser"`
+	DeviceType   string    `json:"device_type"`
+	Os           string    `json:"os"`
 }
