4ffd23243fc3a98dcf0281972c87a378e445a4be
Ярослав <yariko-slav@yandex.ru>
2025-04-22 23:14:01 +0300

    tests(shortener): fix converter_test

--- shortener/tests/converter_test.go
+++ shortener/tests/converter_test.go
@@ -6,104 +6,280 @@ import (
 	"xlink/common/gen/shortener"
 	"xlink/shortener/internal/models"
 	"xlink/shortener/internal/service/helper"
+	"xlink/shortener/internal/service/utils"
 
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
-func TestLinkModelFromLinkRequest(t *testing.T) {
+func TestLinkResponseFromLinkModel(t *testing.T) {
+
+	gen := true
+	shortLink := "qwerty"
+	expireAt := time.Now()
+
+	model := models.Link{
+		Id:        uuid.New(),
+		UserId:    uuid.New(),
+		Generated: &gen,
+		ShortLink: &shortLink,
+		TargetUrl: "https://syubfugsebfuyegfbyu",
+		CreatedAt: time.Now(),
+		ExpireAt:  &expireAt,
+	}
+
+	resp := helper.LinkResponseFromLinkModel(model)
+
+	assert.Equal(t, model.Id.String(), resp.LinkId)
+	assert.Equal(t, model.UserId.String(), resp.UserId)
+	assert.Equal(t, model.ShortLink, resp.ShortLink)
+	assert.Equal(t, model.TargetUrl, resp.TargetUrl)
+	assert.Equal(t, *timestamppb.New(model.CreatedAt), resp.CreatedAt)
+	assert.Equal(t, *timestamppb.New(expireAt), resp.ExpireAt)
+}
+
+func TestLinkModelFromLinkCreateRequest_with_ready_shortlink(t *testing.T) {
 
 	userIDStr := "f9e71cb4-e1e1-4721-8eef-806338db7282"
-	groupIDStr := "f9e71cb4-e1e1-4721-8eef-806338db1111"
-	expireAtStr := "2025-04-16T11:28:07+03:00"
+	shortLinkStr := "http://qwerty"
+
+	testCreateLinkRequest := shortener.CreateLinkRequest{
+		UserId:    userIDStr,
+		ShortLink: &shortLinkStr,
+		TargetUrl: "http://qwertysdijvnisdnc",
+	}
 
 	userUUID, err := uuid.Parse(userIDStr)
 	require.NoError(t, err)
 
-	groupUUID, err := uuid.Parse(groupIDStr)
-	require.NoError(t, err)
-
-	var createdAtTime time.Time
-
+	expireAtStr := "2025-04-16T11:28:07+03:00"
 	expireAtTime, err := time.Parse(time.RFC3339, expireAtStr)
 	require.NoError(t, err)
 
-	testRequest := shortener.UpdateLinkRequest{
-		UserId:    userIDStr,
-		GroupId:   &groupIDStr,
-		Generated: true,
-		ShortLink: "http://qwerty",
-		Url:       "http://qwertysdijvnisdnc",
-		ExpireAt:  &expireAtStr,
-	}
+	generated := false
 
-	expectedModel := &models.Link{
+	expectedModel := models.Link{
 		UserId:    userUUID,
-		GroupId:   &groupUUID,
-		Generated: true,
-		ShortLink: "http://qwerty",
+		ShortLink: &shortLinkStr,
 		TargetUrl: "http://qwertysdijvnisdnc",
-		CreatedAt: createdAtTime,
-		ExpireAt:  expireAtTime,
+		ExpireAt:  &expireAtTime,
+		Generated: &generated,
 	}
 
-	testResponse, err := helper.LinkModelFromLinkRequest(&testRequest, time.Now())
+	testResponse, err := helper.LinkModelFromLinkCreateRequest(&testCreateLinkRequest, time.Now())
 	if err != nil {
 		t.Fatalf("Unable to get link model, %v", err)
 	}
 
-	assert.Equal(t, expectedModel, testResponse)
+	assert.Equal(t, expectedModel.UserId, testResponse.UserId)
+	assert.Equal(t, expectedModel.ShortLink, testResponse.ShortLink)
+	assert.Equal(t, expectedModel.TargetUrl, testResponse.TargetUrl)
+	assert.Equal(t, expireAtTime, testResponse.CreatedAt)
+	assert.Equal(t, *expectedModel.Generated, *testResponse.Generated)
 
 }
 
-func TestLinkModelFromLinkRequestWithId(t *testing.T) {
+func TestLinkModelFromLinkCreateRequest_without_ready_shortlink(t *testing.T) {
+
+	userIDStr := "f9e71cb4-e1e1-4721-8eef-806338db7282"
+	shortLinkStr := ""
+
+	testCreateLinkRequest := shortener.CreateLinkRequest{
+		UserId:    userIDStr,
+		ShortLink: &shortLinkStr,
+		TargetUrl: "http://qwertysdijvnisdnc",
+	}
+
+	userUUID, err := uuid.Parse(userIDStr)
+	require.NoError(t, err)
+
+	expireAtStr := "2025-04-16T11:28:07+03:00"
+	expireAtTime, err := time.Parse(time.RFC3339, expireAtStr)
+	require.NoError(t, err)
+
+	generated := true
+
+	shortLinkForModel := "nsuidfhuyefbuye"
+
+	expectedModel := models.Link{
+		UserId:    userUUID,
+		ShortLink: &shortLinkForModel,
+		TargetUrl: "http://qwertysdijvnisdnc",
+		ExpireAt:  &expireAtTime,
+		Generated: &generated,
+	}
+
+	testResponse, err := helper.LinkModelFromLinkCreateRequest(&testCreateLinkRequest, time.Now())
+	if err != nil {
+		t.Fatalf("Unable to get link model, %v", err)
+	}
+
+	assert.Equal(t, expectedModel.UserId, testResponse.UserId)
+	assert.NotEqual(t, *expectedModel.ShortLink, *testResponse.ShortLink)
+	assert.Equal(t, expectedModel.TargetUrl, testResponse.TargetUrl)
+	assert.Equal(t, expireAtTime, testResponse.CreatedAt)
+	assert.Equal(t, *expectedModel.Generated, *testResponse.Generated)
+
+}
+
+func TestLinkModelFromLinkUpdateRequest_with_regenarating(t *testing.T) {
 
 	idStr := "f9e71cb4-e1e1-4721-8eef-806338db2222"
 	userIDStr := "f9e71cb4-e1e1-4721-8eef-806338db7282"
-	groupIDStr := "f9e71cb4-e1e1-4721-8eef-806338db1111"
+	shortLinkStr := "http://qwerty"
 	expireAtStr := "2025-04-16T11:28:07+03:00"
 
+	expireAtTime, err := time.Parse(time.RFC3339, expireAtStr)
+	require.NoError(t, err)
+
+	expireAtTimestamppb := timestamppb.New(expireAtTime)
+	require.NoError(t, err)
+
+	regenerated := true
+
+	testUpdateLinkRequest := shortener.UpdateLinkRequest{
+		LinkId:     idStr,
+		UserId:     userIDStr,
+		Regenerate: regenerated,
+		ShortLink:  &shortLinkStr,
+		TargetUrl:  "http://qwertysdijvnisdnc",
+		ExpireAt:   expireAtTimestamppb,
+	}
+
+	createdAtStr := "2025-04-16T11:28:06+03:00"
 	idUUID, err := uuid.Parse(idStr)
 	require.NoError(t, err)
 
 	userUUID, err := uuid.Parse(userIDStr)
 	require.NoError(t, err)
 
-	groupUUID, err := uuid.Parse(groupIDStr)
+	createdAtTime, err := time.Parse(time.RFC3339, createdAtStr)
 	require.NoError(t, err)
 
-	var createdAtTime time.Time
+	newShortLink := utils.GenerateShortURL()
 
-	expireAtTime, err := time.Parse(time.RFC3339, expireAtStr)
-	require.NoError(t, err)
-
-	testRequest := shortener.UpdateLinkRequest{
-		Id:        idStr,
-		UserId:    userIDStr,
-		GroupId:   &groupIDStr,
-		Generated: true,
-		ShortLink: "http://qwerty",
-		Url:       "http://qwertysdijvnisdnc",
-		ExpireAt:  &expireAtStr,
-	}
-
-	expectedModel := &models.Link{
+	expectedModel := models.Link{
 		Id:        idUUID,
 		UserId:    userUUID,
-		GroupId:   &groupUUID,
-		Generated: true,
-		ShortLink: "http://qwerty",
+		Generated: &regenerated,
+		ShortLink: &newShortLink,
 		TargetUrl: "http://qwertysdijvnisdnc",
 		CreatedAt: createdAtTime,
-		ExpireAt:  expireAtTime,
+		ExpireAt:  &expireAtTime,
 	}
 
-	testResponse, err := helper.LinkModelFromLinkRequestWithId(&testRequest, time.Now())
+	testResponse, err := helper.LinkModelFromLinkUpdateRequest(&testUpdateLinkRequest)
 	if err != nil {
 		t.Fatalf("Unable to get link model, %v", err)
 	}
 
-	assert.Equal(t, expectedModel, testResponse)
+	assert.Equal(t, expectedModel.Id, testResponse.Id)
+	assert.Equal(t, expectedModel.UserId, testResponse.UserId)
+	assert.NotEqual(t, *expectedModel.ShortLink, *testResponse.ShortLink)
+	assert.Equal(t, expectedModel.TargetUrl, testResponse.TargetUrl)
+	assert.Equal(t, expireAtTime, testResponse.CreatedAt)
+	assert.Equal(t, *expectedModel.Generated, *testResponse.Generated)
 
+}
+func TestLinkModelFromLinkUpdateRequest_without_regenarating(t *testing.T) {
+
+	idStr := "f9e71cb4-e1e1-4721-8eef-806338db2222"
+	userIDStr := "f9e71cb4-e1e1-4721-8eef-806338db7282"
+	shortLinkStr := "http://qwerty"
+	expireAtStr := "2025-04-16T11:28:07+03:00"
+
+	expireAtTime, err := time.Parse(time.RFC3339, expireAtStr)
+	require.NoError(t, err)
+
+	expireAtTimestamppb := timestamppb.New(expireAtTime)
+	require.NoError(t, err)
+
+	regenerated := false
+
+	testUpdateLinkRequest := shortener.UpdateLinkRequest{
+		LinkId:     idStr,
+		UserId:     userIDStr,
+		Regenerate: regenerated,
+		ShortLink:  &shortLinkStr,
+		TargetUrl:  "http://qwertysdijvnisdnc",
+		ExpireAt:   expireAtTimestamppb,
+	}
+
+	createdAtStr := "2025-04-16T11:28:06+03:00"
+	idUUID, err := uuid.Parse(idStr)
+	require.NoError(t, err)
+
+	userUUID, err := uuid.Parse(userIDStr)
+	require.NoError(t, err)
+
+	createdAtTime, err := time.Parse(time.RFC3339, createdAtStr)
+	require.NoError(t, err)
+
+	expectedModel := models.Link{
+		Id:        idUUID,
+		UserId:    userUUID,
+		Generated: &regenerated,
+		ShortLink: &shortLinkStr,
+		TargetUrl: "http://qwertysdijvnisdnc",
+		CreatedAt: createdAtTime,
+		ExpireAt:  &expireAtTime,
+	}
+
+	testResponse, err := helper.LinkModelFromLinkUpdateRequest(&testUpdateLinkRequest)
+	if err != nil {
+		t.Fatalf("Unable to get link model, %v", err)
+	}
+
+	assert.Equal(t, expectedModel.Id, testResponse.Id)
+	assert.Equal(t, expectedModel.UserId, testResponse.UserId)
+	assert.Equal(t, *expectedModel.ShortLink, *testResponse.ShortLink)
+	assert.Equal(t, expectedModel.TargetUrl, testResponse.TargetUrl)
+	assert.Equal(t, expireAtTime, testResponse.CreatedAt)
+	assert.Equal(t, *expectedModel.Generated, *testResponse.Generated)
+
+}
+
+func TestRedirectRequestToClick(t *testing.T) {
+	shortLink := "abc123"
+	clickedAt := timestamppb.Now()
+	referrer := "https://example.com"
+	ipAdress := "127.0.0.1"
+	visitorToken := "segryureh747reh9er9h"
+	userAgent := shortener.UserAgent{Browser: "chrome", DeviceType: "mobile", Os: "android"}
+
+	req := &shortener.RedirectRequest{
+		ShortLink:    shortLink,
+		ClickedAt:    clickedAt,
+		Referrer:     referrer,
+		IpAddress:    ipAdress,
+		VisitorToken: visitorToken,
+		UserAgent:    &userAgent,
+	}
+
+	expectedModel := models.Click{
+		ShortLink:    shortLink,
+		ClickedAt:    clickedAt.AsTime(),
+		Referrer:     referrer,
+		IPAddress:    ipAdress,
+		VisitorToken: visitorToken,
+		Browser:      userAgent.Browser,
+		DeviceType:   userAgent.DeviceType,
+		Os:           userAgent.Os,
+	}
+
+	resp, err := helper.RedirectRequestToClick(req)
+	if err != nil {
+		t.Errorf("failed to make click model:%v", err)
+	}
+
+	assert.Equal(t, expectedModel.ShortLink, resp.ShortLink)
+	assert.Equal(t, expectedModel.ClickedAt, resp.ClickedAt)
+	assert.Equal(t, expectedModel.Referrer, resp.Referrer)
+	assert.Equal(t, expectedModel.IPAddress, resp.IPAddress)
+	assert.Equal(t, expectedModel.VisitorToken, resp.VisitorToken)
+	assert.Equal(t, expectedModel.Browser, resp.Browser)
+	assert.Equal(t, expectedModel.DeviceType, resp.DeviceType)
+	assert.Equal(t, expectedModel.Os, resp.Os)
 }
