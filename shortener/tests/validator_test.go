ee446466359e8fdff6d55e713d4e27499e065632
Ярослав <yariko-slav@yandex.ru>
2025-04-23 00:19:52 +0300

    tests(shortener): fix validator_test

--- shortener/tests/validator_test.go
+++ shortener/tests/validator_test.go
@@ -8,10 +8,12 @@ import (
 
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 func TestGetValidatedId(t *testing.T) {
-	testRequest := shortener.DeleteLinkRequest{Id: "f9e71cb4-e1e1-4721-8eef-806338db2222"}
+	testRequest := shortener.DeleteLinkRequest{LinkId: "f9e71cb4-e1e1-4721-8eef-806338db2222"}
 
 	testResponse, err := helper.GetValidatedId(&testRequest)
 	if err != nil {
@@ -29,22 +31,16 @@ func TestGetValidatedId(t *testing.T) {
 
 func TestGetValidatedUserId(t *testing.T) {
 
-	idStr := "f9e71cb4-e1e1-4721-8eef-806338db2222"
 	userIDStr := "f9e71cb4-e1e1-4721-8eef-806338db7282"
-	groupIDStr := "f9e71cb4-e1e1-4721-8eef-806338db1111"
-	expireAtStr := "2025-04-16T11:28:07+03:00"
+	shortLinkStr := "http://qwerty"
 
-	testRequest := shortener.UpdateLinkRequest{
-		Id:        idStr,
+	testCreateLinkRequest := shortener.CreateLinkRequest{
 		UserId:    userIDStr,
-		GroupId:   &groupIDStr,
-		Generated: true,
-		ShortLink: "http://qwerty",
-		Url:       "http://qwertysdijvnisdnc",
-		ExpireAt:  &expireAtStr,
+		ShortLink: &shortLinkStr,
+		TargetUrl: "http://qwertysdijvnisdnc",
 	}
 
-	testResponse, err := helper.GetValidatedUserId(&testRequest)
+	testResponse, err := helper.GetValidatedUserId(&testCreateLinkRequest)
 	if err != nil {
 		t.Fatalf("Unable to get id, %v", err)
 	}
@@ -58,117 +54,127 @@ func TestGetValidatedUserId(t *testing.T) {
 
 }
 
-func TestGetValidatedGroupId(t *testing.T) {
-
-	idStr := "f9e71cb4-e1e1-4721-8eef-806338db2222"
-	userIDStr := "f9e71cb4-e1e1-4721-8eef-806338db7282"
-	groupIDStr := "f9e71cb4-e1e1-4721-8eef-806338db1111"
-	expireAtStr := "2025-04-16T11:28:07+03:00"
-
-	testRequest := shortener.UpdateLinkRequest{
-		Id:        idStr,
-		UserId:    userIDStr,
-		GroupId:   &groupIDStr,
-		Generated: true,
-		ShortLink: "http://qwerty",
-		Url:       "http://qwertysdijvnisdnc",
-		ExpireAt:  &expireAtStr,
-	}
-
-	testResponse, err := helper.GetValidatedGroupId(&testRequest, nil)
-	if err != nil {
-		t.Fatalf("Unable to get id, %v", err)
-	}
-
-	parsed, err := uuid.Parse(groupIDStr)
-	expected := &parsed
-	if err != nil {
-		t.Fatalf("Unable to parse id, %v", err)
-	}
-
-	assert.Equal(t, expected, testResponse)
-
-}
-
 func TestGetValidatedExpireAt(t *testing.T) {
 
 	idStr := "f9e71cb4-e1e1-4721-8eef-806338db2222"
 	userIDStr := "f9e71cb4-e1e1-4721-8eef-806338db7282"
-	groupIDStr := "f9e71cb4-e1e1-4721-8eef-806338db1111"
+	shortLinkStr := "http://qwerty"
 	expireAtStr := "2025-04-16T11:28:07+03:00"
+	expireAtStr1 := "2025-04-16T11:28:08+03:00"
+
+	expireAtTime, err := time.Parse(time.RFC3339, expireAtStr)
+	require.NoError(t, err)
+	expireAtTime1, err := time.Parse(time.RFC3339, expireAtStr1)
+	require.NoError(t, err)
+
+	expireAtTimestamppb := timestamppb.New(expireAtTime)
+	require.NoError(t, err)
+
+	regenerated := true
 
 	testRequest := shortener.UpdateLinkRequest{
-		Id:        idStr,
-		UserId:    userIDStr,
-		GroupId:   &groupIDStr,
-		Generated: true,
-		ShortLink: "http://qwerty",
-		Url:       "http://qwertysdijvnisdnc",
-		ExpireAt:  &expireAtStr,
+		LinkId:     idStr,
+		UserId:     userIDStr,
+		Regenerate: regenerated,
+		ShortLink:  &shortLinkStr,
+		TargetUrl:  "http://qwertysdijvnisdnc",
+		ExpireAt:   expireAtTimestamppb,
 	}
 
-	testResponse, err := helper.GetValidatedExpireAt(&testRequest, time.Now())
+	testResponse, err := helper.GetValidatedExpireAt(&testRequest, expireAtTime1)
 	if err != nil {
 		t.Fatalf("Unable to get ExpireAt, %v", err)
 	}
 
-	expected, err := time.Parse(time.RFC3339, *testRequest.ExpireAt)
-	if err != nil {
-		t.Fatalf("Unable to parse ExpireAt, %v", err)
-	}
+	expected := expireAtStr
 
 	assert.Equal(t, expected, testResponse)
 
 }
 
-func TestValidatedStringNotEmpty(t *testing.T) {
+func TestValidatedStringNotEmpty_with_no_empty_string(t *testing.T) {
 
-	idStr := "f9e71cb4-e1e1-4721-8eef-806338db2222"
-	userIDStr := "f9e71cb4-e1e1-4721-8eef-806338db7282"
-	groupIDStr := "f9e71cb4-e1e1-4721-8eef-806338db1111"
-	expireAtStr := "2025-04-16T11:28:07+03:00"
+	str := "srgfuiybesuifh"
 
-	testRequest := shortener.UpdateLinkRequest{
-		Id:        idStr,
-		UserId:    userIDStr,
-		GroupId:   &groupIDStr,
-		Generated: true,
-		ShortLink: "http://qwerty",
-		Url:       "http://qwertysdijvnisdnc",
-		ExpireAt:  &expireAtStr,
-	}
+	resp := helper.ValidateStringNotEmpty(str)
 
-	testerror := helper.ValidateStringNotEmpty(testRequest.GetShortLink())
-	if testerror != nil {
-		t.Fatalf("ValidateStringNotEmpty error, %v", testerror)
-	}
+	assert.NoError(t, resp)
 
-	assert.NoError(t, testerror)
+}
+func TestValidatedStringNotEmpty_with_empty_string(t *testing.T) {
+
+	str := ""
+
+	resp := helper.ValidateStringNotEmpty(str)
+
+	assert.Error(t, resp)
 
 }
 
-func TestValidatedUrl(t *testing.T) {
+func TestValidatedUrl_with_no_empty_url(t *testing.T) {
 
-	idStr := "f9e71cb4-e1e1-4721-8eef-806338db2222"
-	userIDStr := "f9e71cb4-e1e1-4721-8eef-806338db7282"
-	groupIDStr := "f9e71cb4-e1e1-4721-8eef-806338db1111"
-	expireAtStr := "2025-04-16T11:28:07+03:00"
+	url := "https://shbvgfuyesbfuybe"
 
-	testRequest := shortener.UpdateLinkRequest{
-		Id:        idStr,
-		UserId:    userIDStr,
-		GroupId:   &groupIDStr,
-		Generated: true,
-		ShortLink: "http://qwerty",
-		Url:       "http://qwertysdijvnisdnc",
-		ExpireAt:  &expireAtStr,
+	resp := helper.ValidateUrl(url)
+
+	assert.NoError(t, resp)
+}
+
+func TestValidatedUrl_with_empty_url(t *testing.T) {
+
+	url := ""
+
+	resp := helper.ValidateUrl(url)
+
+	assert.Error(t, resp)
+}
+
+func TestValidateShortLink_no_empty_string(t *testing.T) {
+	str := "nsurgfhbsuyfb"
+
+	resp, err := helper.ValidateShortLink(str)
+	if resp == "" {
+		t.Error("uxexpected empty string")
 	}
 
-	testerror := helper.ValidateUrl(testRequest.GetUrl())
-	if testerror != nil {
-		t.Fatalf("ValidateUrl error, %v", testerror)
+	assert.NoError(t, err)
+}
+
+func TestValidateShortLink_with_empty_string(t *testing.T) {
+	str := ""
+
+	_, err := helper.ValidateShortLink(str)
+
+	assert.Error(t, err)
+}
+
+func TestValidateIPAddress_correct(t *testing.T) {
+	ip := "1.1.1.11.1."
+
+	resp, err := helper.ValidateIPAddress(ip)
+	if resp == "" {
+		t.Error("uxexpected empty ip")
 	}
 
-	assert.NoError(t, testerror)
+	assert.NoError(t, err)
 
+}
+
+func TestValidateNotEmptyStr_with_no_empty_string(t *testing.T) {
+	str := "srgfuiybesuifh"
+
+	resp, err := helper.ValidateNotEmptyStr(str)
+	if resp == "" {
+		t.Error("uxexpected empty string")
+	}
+
+	assert.NoError(t, err)
+}
+
+func TestValidateNotEmptyStr_with_empty_string(t *testing.T) {
+	str := ""
+
+	_, err := helper.ValidateNotEmptyStr(str)
+
+	assert.NoError(t, err)
 }
