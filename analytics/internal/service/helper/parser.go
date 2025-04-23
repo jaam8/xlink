9e75dabbe3fd9014d883bd56ac25568342b5fb73
jaam8 <jaam8@yandex.ru>
2025-04-23 19:19:15 +0700

    fix(analytics): fix linter errors

--- analytics/internal/service/helper/parser.go
+++ analytics/internal/service/helper/parser.go
@@ -19,13 +19,13 @@ func ParseRegionAndCountry(ip string) (string, string, error) {
 	var info struct {
 		Country string `json:"country"`
 		Region  string `json:"region"`
-		bogon   bool   `json:"bogon,omitempty"`
+		Bogon   bool   `json:"bogon,omitempty"`
 	}
 	if err = json.NewDecoder(resp.Body).Decode(&info); err != nil {
 		return "", "", err
 	}
 
-	if info.bogon || info.Country == "" || info.Region == "" {
+	if info.Bogon || info.Country == "" || info.Region == "" {
 		return "Unknown", "Unknown", nil
 	}
 	return info.Region, info.Country, nil
