b2e6d62bdaa152c576d7ffd98be90b5e5f697779
jaam8 <jaam8@yandex.ru>
2025-04-23 18:34:53 +0700

    feat(analytics): add responses models and update Click model

--- analytics/internal/models/click.go
+++ analytics/internal/models/click.go
@@ -6,15 +6,17 @@ import (
 )
 
 type Click struct {
-	LinkOwner  uuid.UUID `json:"link_owner"`
-	ShortLink  string    `json:"short_link"`
-	ClickedAt  time.Time `json:"clicked_at"`
-	Referrer   string    `json:"referrer"`
-	Region     string    `json:"region"`
-	Browser    string    `json:"browser"`
-	DeviceType string    `json:"device_type"`
-	IPAddress  string    `json:"ip_address"`
-	IsUnique   bool      `json:"is_unique"`
+	LinkOwner  uuid.UUID `json:"link_owner" ch:"link_owner"`
+	ShortLink  string    `json:"short_link" ch:"short_link"`
+	ClickedAt  time.Time `json:"clicked_at" ch:"clicked_at"`
+	Referrer   string    `json:"referrer" ch:"referrer"`
+	Country    string    `json:"country" ch:"country"`
+	Region     string    `json:"region" ch:"region"`
+	Browser    string    `json:"browser" ch:"browser"`
+	DeviceType string    `json:"device_type" ch:"device_type"`
+	Os         string    `json:"os" ch:"os"`
+	IPAddress  string    `json:"ip_address" ch:"ip_address"`
+	IsUnique   bool      `json:"is_unique" ch:"is_unique"`
 }
 
 type ClickEvent struct {
