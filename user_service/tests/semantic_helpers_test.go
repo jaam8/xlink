c09c1361a1c7ce1c8d70cdbe1510b364de1004cf
Ярослав <yariko-slav@yandex.ru>
2025-04-23 22:10:05 +0300

    tests(user_service): remove !ok staticcheck from sem_helpers_test

--- user_service/tests/semantic_helpers_test.go
+++ user_service/tests/semantic_helpers_test.go
@@ -8,48 +8,36 @@ import (
 )
 
 func TestGetRoleByIsStaffIsAdmin(t *testing.T) {
-	ok := t.Run("superuser case", func(t *testing.T) {
+	t.Run("superuser case", func(t *testing.T) {
 		isStaff := true
 		isAdmin := true
 		resp := utils.GetRoleByIsStaffIsAdmin(isStaff, isAdmin)
 
 		assert.Equal(t, "superuser", resp)
 	})
-	if !ok {
-		return
-	}
 
-	ok = t.Run("admin case", func(t *testing.T) {
+	t.Run("admin case", func(t *testing.T) {
 		isStaff := false
 		isAdmin := true
 		resp := utils.GetRoleByIsStaffIsAdmin(isStaff, isAdmin)
 
 		assert.Equal(t, "admin", resp)
 	})
-	if !ok {
-		return
-	}
 
-	ok = t.Run("staff case", func(t *testing.T) {
+	t.Run("staff case", func(t *testing.T) {
 		isStaff := true
 		isAdmin := false
 		resp := utils.GetRoleByIsStaffIsAdmin(isStaff, isAdmin)
 
 		assert.Equal(t, "staff", resp)
 	})
-	if !ok {
-		return
-	}
 
-	ok = t.Run("user case", func(t *testing.T) {
+	t.Run("user case", func(t *testing.T) {
 		isStaff := false
 		isAdmin := false
 		resp := utils.GetRoleByIsStaffIsAdmin(isStaff, isAdmin)
 
 		assert.Equal(t, "user", resp)
 	})
-	if !ok {
-		return
-	}
 
 }
