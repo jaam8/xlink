e27f6826e405b865a09ff94958c90f7aadde4068
Danis <dandijar@yandex.ru>
2025-04-24 16:24:27 +0500

    fix(gateway): use /get /create /update /delete instead of /crud

--- gateway/cmd/main.go
+++ gateway/cmd/main.go
@@ -147,19 +147,19 @@ func main() {
 	userStaffGroup.Use(isStaffMiddleware)
 	userAuthedGroup.Use(authMiddleware)
 
-	userGroup.Post("/crud", userServiceHandler.CreateUser)
-	userGroup.Patch("/crud/:id", userServiceHandler.UpdateUser)
+	userGroup.Post("/create", userServiceHandler.CreateUser)
+	userGroup.Patch("/update/:id", userServiceHandler.UpdateUser)
 	userGroup.Post("/refresh", userServiceHandler.RefreshToken)
 	userGroup.Post("/login", userServiceHandler.Login)
 
-	userStaffGroup.Get("/crud/:id", userServiceHandler.GetUser)                    // staff | admin
+	userStaffGroup.Get("/get/:id", userServiceHandler.GetUser)                     // staff | admin
 	userStaffGroup.Get("/get-by-tg-id/:tg_id", userServiceHandler.GetUserIdByTgId) // staff | admin
-	userStaffGroup.Delete("/crud/:id", userServiceHandler.DeleteUser)              // staff | admin
+	userStaffGroup.Delete("/delete/:id", userServiceHandler.DeleteUser)            // staff | admin
 	userStaffGroup.Get("/role/:id", userServiceHandler.GetRole)                    // staff | admin
 
-	userAdminGroup.Post("/crud", userServiceHandler.CreateUserAdmin)          // admin
-	userAdminGroup.Patch("/crud/:id", userServiceHandler.UpdateUserAdmin)     // admin
-	userAdminGroup.Delete("/crud/:id", userServiceHandler.DeleteUserAdmin)    // admin
+	userAdminGroup.Post("/create", userServiceHandler.CreateUserAdmin)        // admin
+	userAdminGroup.Patch("/update/:id", userServiceHandler.UpdateUserAdmin)   // admin
+	userAdminGroup.Delete("/delete/:id", userServiceHandler.DeleteUserAdmin)  // admin
 	userAdminGroup.Post("/get-by-token", userServiceHandler.GetUserIDByToken) // admin
 	userAdminGroup.Post("/token-check", userServiceHandler.CheckToken)        // admin
 
@@ -179,12 +179,12 @@ func main() {
 	shortenerOwnerOnlyGroup.Use(middlewares.ShortenerOwnerOnlyMiddleware("id", shortenerService))
 
 	//TODO: id -> shortLink
-	app.Get("/l/:shortLink", shortenerServiceHandler.Redirect)                      //
-	shortenerCRUDGroup.Post("/crud", shortenerServiceHandler.CreateNewLink)         // authenticated
-	shortenerOwnerOnlyGroup.Put("/crud/:id", shortenerServiceHandler.UpdateLink)    // owner
-	shortenerOwnerOnlyGroup.Delete("/crud/:id", shortenerServiceHandler.DeleteLink) // owner
-	shortenerAdminGroup.Put("/crud/:id", shortenerServiceHandler.UpdateLink)        // admin
-	shortenerAdminGroup.Delete("/crud/:id", shortenerServiceHandler.DeleteLink)     // admin
+	app.Get("/l/:shortLink", shortenerServiceHandler.Redirect)                        //
+	shortenerCRUDGroup.Post("/create", shortenerServiceHandler.CreateNewLink)         // authenticated
+	shortenerOwnerOnlyGroup.Put("/update/:id", shortenerServiceHandler.UpdateLink)    // owner
+	shortenerOwnerOnlyGroup.Delete("/delete/:id", shortenerServiceHandler.DeleteLink) // owner
+	shortenerAdminGroup.Put("/update/:id", shortenerServiceHandler.UpdateLink)        // admin
+	shortenerAdminGroup.Delete("/delete/:id", shortenerServiceHandler.DeleteLink)     // admin
 	//endregion shortener v1
 
 	//region analytics v1
