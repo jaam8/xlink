e9f8f679ad81a5c36282a3d412cdfe6f7e7f2ed5
Danis <dandijar@yandex.ru>
2025-04-27 00:00:53 +0500

    fix(tg_bot): fix renderer & analytics part

--- tg_bot/internal/ports/adapters/renderer_adapter/gateway.go
+++ tg_bot/internal/ports/adapters/renderer_adapter/gateway.go
@@ -18,11 +18,12 @@ func (s *RendererAdapterGateway) RenderChart(chartType string) (string, error) {
 }
 
 func (s *RendererAdapterGateway) GetImageUrl(shortLink, token, param string, startDate time.Time, endDate time.Time) string {
-	return fmt.Sprintf("%s/%s/?token=%s&param=%s&start_date=%s&end_date=%s",
+	url := fmt.Sprintf("%s%s/?token=%s&param=%s&start_date=%s&end_date=%s",
 		s.BaseUrl,
 		shortLink,
 		token, param,
 		startDate.Format(time.DateOnly),
 		endDate.Format(time.DateOnly),
 	)
+	return url
 }
