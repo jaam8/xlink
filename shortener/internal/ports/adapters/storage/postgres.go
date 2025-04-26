718225de9849fa6b153010ff0997498f46b9e42c
Danis <dandijar@yandex.ru>
2025-04-26 16:34:07 +0500

    fix(shortener): remove links count query in GetLinks
    - no goroutines -> everything works

--- shortener/internal/ports/adapters/storage/postgres.go
+++ shortener/internal/ports/adapters/storage/postgres.go
@@ -3,7 +3,6 @@ package storage
 import (
 	"context"
 	"fmt"
-	"sync"
 	"xlink/shortener/internal/models"
 
 	"github.com/Masterminds/squirrel"
@@ -38,16 +37,6 @@ func ShortLinkSelectQuery(filter squirrel.Eq) (string, []interface{}, error) {
 	return sql, args, err
 }
 
-func LinkCountQuery(filter squirrel.Eq) (string, []interface{}, error) {
-	sql, args, err := squirrel.Select("count(*)").
-		From("shortener.urls").
-		Where(filter).
-		PlaceholderFormat(squirrel.Dollar).
-		ToSql()
-
-	return sql, args, err
-}
-
 func NewShortenerStorageRepositoryPostgres(pool *pgxpool.Pool) *ShortenerStorageRepositoryPostgres {
 	return &ShortenerStorageRepositoryPostgres{
 		PostgresPool: pool,
@@ -55,30 +44,6 @@ func NewShortenerStorageRepositoryPostgres(pool *pgxpool.Pool) *ShortenerStorage
 }
 
 func (s *ShortenerStorageRepositoryPostgres) GetLinks(userId uuid.UUID) ([]string, error) {
-	//region getCount
-	wg := new(sync.WaitGroup)
-	wg.Add(1)
-
-	countChannel := make(chan uint64, 1)
-	countErrorChannel := make(chan error, 1)
-	go func() {
-		defer wg.Done()
-
-		countSql, countArgs, err := LinkCountQuery(squirrel.Eq{"user_id": userId})
-		if err != nil {
-			countErrorChannel <- fmt.Errorf("couldn't build SQL rows count query: %w", err)
-		}
-
-		var totalRows uint64
-		err = s.PostgresPool.QueryRow(context.Background(), countSql, countArgs...).Scan(&totalRows)
-		if err != nil {
-			countErrorChannel <- fmt.Errorf("couldn't execute get rows count query: %w", err)
-		}
-
-		countChannel <- totalRows
-	}()
-	//endregion getCount
-
 	sql, args, err := ShortLinkSelectQuery(squirrel.Eq{"user_id": userId})
 	if err != nil {
 		return []string{}, fmt.Errorf("couldn't build an SQL query: %w", err)
@@ -89,16 +54,9 @@ func (s *ShortenerStorageRepositoryPostgres) GetLinks(userId uuid.UUID) ([]strin
 		return []string{}, fmt.Errorf("couldn't execute select link list query: %w", err)
 	}
 
-	wg.Wait()
-	countRowsError := <-countErrorChannel
-	if countRowsError != nil {
-		return []string{}, countRowsError
-	}
-	totalRows := <-countChannel
-
 	defer rows.Close()
 
-	links := make([]string, 0, totalRows)
+	links := make([]string, 0)
 
 	for rows.Next() {
 		var link string
